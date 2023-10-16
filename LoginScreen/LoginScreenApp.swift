1803046fd2a56f26aebcca4884351e5a65862d92
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-16 20:09:21 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Bekir Berke Yılmaz on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
